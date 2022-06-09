1c5d07c418877b597098d27138ff267a84f49dbb
wjlee611 <wjlee611@gmail.com>
2022-06-09 14:21:48 +0900

    add styles

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
   const [loading, setLoading] = useState(true);
@@ -25,34 +26,41 @@ function Home() {
   }, []);
   return (
     <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          <h2>set min rating ({rating})</h2>
-          <input
-            style={{ width: "12rem" }}
-            type="range"
-            min={0}
-            max={10}
-            step={0.5}
-            defaultValue={rating}
-            onChange={onChange}
-            onMouseUp={onMouseUp}
-          />
-          <hr />
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              id={movie.id}
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-            />
-          ))}
-        </div>
-      )}
+      <div className={styles.rate_slider}>
+        <h2>Set min rating - {rating}</h2>
+        <input
+          style={{ width: "12rem" }}
+          type="range"
+          min={0}
+          max={9}
+          step={0.5}
+          defaultValue={rating}
+          onChange={onChange}
+          onMouseUp={onMouseUp}
+        />
+      </div>
+      <hr />
+      <div className={styles.container}>
+        {loading ? (
+          <div className={styles.loader}>
+            <span>Loading...</span>
+          </div>
+        ) : (
+          <div className={styles.movies}>
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                coverImg={movie.medium_cover_image}
+                title={movie.title}
+                summary={movie.summary}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
